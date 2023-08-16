3e2a300bfceaed82a3ca05aeedf4121854f26f01
jeohar <jeohar@outlook.com>
2023-08-15 17:12:52 -0700

    New code added

--- src/tek/demo/MyClass.java
+++ src/tek/demo/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("This is Demo project for Clone");
 		System.out.println("This line is added by Johar");
-		System.out.println("This line is added by Johar");
+		
 	}
 
 }
